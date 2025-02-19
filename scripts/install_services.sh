b7f68544e90d398fa3ab44139d70b2640bc42a09
Diego Ordonez <diegordonez4@gmail.com>
2025-02-19 08:54:24 +0100

    Simplify Docker installation in install_services.sh by using the official installation script

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -47,16 +47,8 @@ fi
 # Install Docker if not installed
 if ! command -v docker &>/dev/null; then
     echo "Instalando Docker..."
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
     echo "Docker instalado correctamente. Ejecutando prueba..."
     sudo docker run hello-world
 else
