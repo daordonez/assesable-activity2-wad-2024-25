01ee7c422e34894f78f6714083cd2420f752c487
Diego Ordonez <diegordonez4@gmail.com>
2025-02-19 09:02:19 +0100

    Refactor Docker installation in install_services.sh to use official repository and improve security

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -47,8 +47,19 @@ fi
 # Install Docker if not installed
 if ! command -v docker &>/dev/null; then
     echo "Instalando Docker..."
-    curl -fsSL https://get.docker.com | sudo sh
-    sudo usermod -aG docker $USER
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     echo "Docker instalado correctamente. Ejecutando prueba..."
     sudo docker run hello-world
 else
